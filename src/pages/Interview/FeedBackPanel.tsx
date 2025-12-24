9af88a45b790cce571abd2249be18d616b17a797
Saravana Priyan <c.saravanapriyan@gmail.com>
2025-12-24 15:58:14 +0530

    tweaks in css feedbacknaming

--- src/pages/Interview/FeedBackPanel.tsx
+++ src/pages/Interview/FeedBackPanel.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../styles/FeedbackPanel.css";
+import "../../styles/FeedBackPanel.css";
 import { exportFeedbackPdf } from "../../utils/exportFeedbackPdf";
 
 interface Feedback {
